10612275ffb1c76488a0c41ed30f0137eee76703
Nasir Rabbani <nrabbani@tenable.com>
2023-05-01 06:56:31 +0530

    removed commented backend

--- backend.tf
+++
@@ -1,7 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "tcs-engineering-tfstatestore-bucket-0587"
-#     key    = "tfstates/acqa-repo1-aws-tf12-part1-no-name.tfstate"
-#     region = "us-east-1"
-#   }
-# }
